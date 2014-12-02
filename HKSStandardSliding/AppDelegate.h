acc40baec03e4afa1f2166ccdebcd089f66c001b
Ke Song <800578@gmail.com>
2014-12-02 14:15:31 +0100

    neu initialized

---
+++ HKSStandardSliding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HKSStandardSliding
+//
+//  Created by Ke Song on 02.12.14.
+//  Copyright (c) 2014 Ke Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HKSStandardSliding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HKSStandardSliding
+//
+//  Created by Ke Song on 02.12.14.
+//  Copyright (c) 2014 Ke Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
